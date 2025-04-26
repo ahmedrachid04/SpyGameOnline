d81ebea0badf904968a2394d975d6fb80d6fc5cc
ahmedrachid04 <ahmedrachid0409@gmail.com>
2025-04-27 01:10:15 +0200

    yaarbi t5dem

--- odd-one-out-multiplayer/src/services/socket.js
+++ odd-one-out-multiplayer/src/services/socket.js
@@ -2,6 +2,6 @@
 
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000"); //port dyal server dyal node.js
+const socket = io("https://middos-spy-game.onrender.com"); //port dyal server dyal node.js
 
 export default socket;
