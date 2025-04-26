da0bc41df1bb830c917cf54dedc3940a0ccbdd0f
ahmedrachid04 <ahmedrachid0409@gmail.com>
2025-04-27 01:32:05 +0200

    sawbna socket

--- odd-one-out-multiplayer/src/services/socket.js
+++ odd-one-out-multiplayer/src/services/socket.js
@@ -2,6 +2,6 @@
 
 import { io } from "socket.io-client";
 
-const socket = io("https://middos-spy-game.onrender.com"); //port dyal server dyal node.js
+const socket = io("https://middos-spy-game.onrender.com"); //url dyal back dyal nodejs
 
 export default socket;
